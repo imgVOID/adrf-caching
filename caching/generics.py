407d4681f5eb11e8141d2ef95cfadae6a74587d4
imgvoid <imgvoid@gmail.com>
2026-01-04 16:35:43 +0200

    Implement async methods for CRUD operations in generics.py

--- caching/generics.py
+++ caching/generics.py
@@ -9,12 +9,17 @@ class CreateAPIView(mixins.CreateModelMixin, GenericAPIView):
     Concrete async cached view for creating a model instance.
     """
 
+    async def post(self, request, *args, **kwargs):
+        return await self.acreate(request, *args, **kwargs)
+
 
 class ListAPIView(mixins.ListModelMixin, GenericAPIView):
     """
     Concrete async cached view for listing a queryset.
     """
 
+    async def get(self, request, *args, **kwargs):
+        return await self.alist(request, *args, **kwargs)
 
 
 class RetrieveAPIView(mixins.RetrieveModelMixin, GenericAPIView):
@@ -22,11 +27,17 @@ class RetrieveAPIView(mixins.RetrieveModelMixin, GenericAPIView):
     Concrete async cached view for retrieving a model instance.
     """
 
+    async def get(self, request, *args, **kwargs):
+        return await self.aretrieve(request, *args, **kwargs)
+
 
 class DestroyAPIView(mixins.DestroyModelMixin, GenericAPIView):
     """
     Concrete async cached view for deleting a model instance.
     """
+
+    async def delete(self, request, *args, **kwargs):
+        return await self.adestroy(request, *args, **kwargs)
 
 
 class UpdateAPIView(mixins.UpdateModelMixin, GenericAPIView):
@@ -34,11 +45,23 @@ class UpdateAPIView(mixins.UpdateModelMixin, GenericAPIView):
     Concrete async cached view for updating a model instance.
     """
 
+    async def put(self, request, *args, **kwargs):
+        return await self.aupdate(request, *args, **kwargs)
+
+    async def patch(self, request, *args, **kwargs):
+        return await self.partial_aupdate(request, *args, **kwargs)
+
 
 class ListCreateAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, GenericAPIView):
     """
     Concrete async cached view for listing a queryset or creating a model instance.
     """
+
+    async def get(self, request, *args, **kwargs):
+        return await self.alist(request, *args, **kwargs)
+
+    async def post(self, request, *args, **kwargs):
+        return await self.acreate(request, *args, **kwargs)
 
 
 class RetrieveUpdateAPIView(
@@ -48,6 +71,15 @@ class RetrieveUpdateAPIView(
     Concrete async cached view for retrieving, updating a model instance.
     """
 
+    async def get(self, request, *args, **kwargs):
+        return await self.aretrieve(request, *args, **kwargs)
+
+    async def put(self, request, *args, **kwargs):
+        return await self.aupdate(request, *args, **kwargs)
+
+    async def patch(self, request, *args, **kwargs):
+        return await self.partial_aupdate(request, *args, **kwargs)
+
 
 class RetrieveDestroyAPIView(
     mixins.RetrieveModelMixin, mixins.DestroyModelMixin, GenericAPIView
@@ -55,6 +87,12 @@ class RetrieveDestroyAPIView(
     """
     Concrete async cached view for retrieving or deleting a model instance.
     """
+
+    async def get(self, request, *args, **kwargs):
+        return await self.aretrieve(request, *args, **kwargs)
+
+    async def delete(self, request, *args, **kwargs):
+        return await self.adestroy(request, *args, **kwargs)
 
 
 class RetrieveUpdateDestroyAPIView(
@@ -66,3 +104,16 @@ class RetrieveUpdateDestroyAPIView(
     """
     Concrete async cached view for retrieving, updating or deleting a model instance.
     """
+
+
+    async def get(self, request, *args, **kwargs):
+        return await self.aretrieve(request, *args, **kwargs)
+
+    async def put(self, request, *args, **kwargs):
+        return await self.aupdate(request, *args, **kwargs)
+
+    async def patch(self, request, *args, **kwargs):
+        return await self.partial_aupdate(request, *args, **kwargs)
+
+    async def delete(self, request, *args, **kwargs):
+        return await self.adestroy(request, *args, **kwargs)
